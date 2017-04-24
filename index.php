17c92a4d7f4b7c3cc77ab3d66b63e50b3ac25736
houkanshan <houkanshan@gmail.com>
2017-04-25 02:08:20 +0800

    volumn

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "base.php"; ?>
 
 <?php
-define("VERSION", 14);
+define("VERSION", 15);
 $ip = $_SERVER['REMOTE_ADDR'];
 $is_dead = is_dead();
 $user_id = get_id_by_ip($ip);
--- js/SeamlessLoop.js
+++ js/SeamlessLoop.js
@@ -72,7 +72,7 @@ export default function SeamlessLoop() {
 	this.actual = new Array();
 	this.dropOld = new Boolean();
 	this.old;
-	this._volume = 1;
+	this._volume = 0.5;
 	
 	var t = this;
 	this._eventCanplaythrough = function(audBool) {
