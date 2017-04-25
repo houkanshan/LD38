85d127e5d817b9acaa9aca1d2cefbea193a23646
houmai <houmai@douban.com>
2017-04-25 11:11:46 +0800

    remove console

--- js/index.ts
+++ js/index.ts
@@ -74,15 +74,14 @@ function startPlay() : void {
 
   utils.delayedPromise(300)()
   .then(() => {
-    debugger
-    console.log(GameData)
+    // console.log(GameData)
     if (GameData.canExtend) {
       $.post('extend_life.php')
       updateLifeProgress(GameData.life + 1 / 100)
-      console.info('Life extended.')
+      // console.info('Life extended.')
       GameData.canExtend = false
     } else {
-      console.info('Can`t extend life.')
+      // console.info('Can`t extend life.')
     }
   })
   .then(utils.delayedPromise(800))
